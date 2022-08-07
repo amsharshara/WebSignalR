8d96c5cd2ed72a7f652f606f3866508d439418c7
AM <AM@DESKTOP-TRF2EGL>
2022-08-07 21:09:03 +0300

    Add project files.

---
+++ WebApplication5/Hubs/MsgHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebSignalR.Hubs
+{
+    public class MsgHub:Hub
+    {
+        //create send  msg to all users connect to server
+        public async void SendMsg(string user,string msg )
+        {
+            //"ReceiveMsg" : function name in js file
+            await Clients.All.SendAsync("ReceiveMsg", user, msg );
+        }
+    }
+}
---
+++ WebApplication5/wwwroot/js/chat.js
@@ -0,0 +1,35 @@
+﻿//create client msg
+"user strict";
+
+var conn = new signalR.HubConnectionBuilder().withUrl("/MsgHub").build();
+
+//start connection
+conn.start().then(function () {
+
+    console.log("start");
+
+}).catch(
+    function (err) {
+        console.error(err.toString());
+    }
+);
+
+//send msg to server
+document.getElementById("btnSend").addEventListener("click", function (evt) {
+    var user = document.getElementById("txtUser").value;
+    var msg = document.getElementById("txtMsg").value;
+    conn.invoke("SendMsg", user, msg).catch(function (err) {
+        console.error(err.toString());
+    });
+
+}
+    );
+//receive msg from server
+//write function name
+conn.on("ReceiveMsg", function (user, msg) {
+    var li = document.createElement("li");
+    li.innerHTML = user + ":" + msg;
+    document.getElementById("lstMsg").appendChild(li);
+    console.log(user + ":" + msg);
+
+});
